3183086017d9550ee287d1719928e09e87607542
alexmolchanov <oleksiimolchanov@bitbucket.org>
2024-07-29 13:59:50 +0300

    createSongDTO && validation

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,30 +1,36 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { SongsService } from './songs.service';
+import { CreateSongDTO } from './dto/create-song.dto';
 
 @Controller('songs')
 export class SongsController {
   constructor(private songsService: SongsService) {}
 
-  @Post()
-  create() {
-    return this.songsService.create('Hey you by Pink Floyd')
+  // Create new song
+  @Post() 
+  create(@Body() createSongDTO: CreateSongDTO) {
+    return this.songsService.create(createSongDTO)
   }
 
+  // Get all songs
   @Get()
   findAll() {
     return this.songsService.findAll();
   }
 
+  // Get one song
   @Get(':id')
   findOne() {
     return 'fetch song by id'
   }
 
+  // Update (PUT) song
   @Put(':id')
   update() {
     return 'update song by id'
   }
 
+  // Delete song
   @Delete(':id')
   delete() {
     return 'delete song by id'
